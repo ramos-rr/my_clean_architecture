452580ab172b2e4c9da813d778f3e6721251967f
ramos-rr <ramos-rr@outlook.com>
2022-11-08 15:09:05 -0300

    config: finished test for FindUser with a exception NoResultFoundError

--- src/infra/repo/test/user_repository.py
+++ src/infra/repo/test/user_repository.py
@@ -1,7 +1,7 @@
 from typing import List
 from src.data.interfaces import UserRepositoryInterface
 from src.domain.models import Users
-from src.infra.errors import ErrorManager
+from src.infra.errors import ErrorManager, NoResultFoundError
 from faker import Faker
 from datetime import datetime
 
@@ -12,14 +12,25 @@ now = datetime.now()
 class UserRepositorySpy(UserRepositoryInterface):
     """ Class to mock UserRepository to serve as a test feature in the project """
 
-    @classmethod
-    def insert_user(cls, username: str, password: str) -> Users:
+    def __init__(self,
+                 id=fake.random_number(digits=5),
+                 usename=fake.name(),
+                 password='teste123',
+                 register_date=now,
+                 session=None):
+        self.id = id
+        self.usename = usename
+        self.password = password
+        self.register_date = now
+        self.session = 'test.test.test'
+
+    def insert_user(self, username: str, password: str) -> Users:
         """
         Method to test insertion of a new user
         params: username: New User's name,
         params: password: New user's password
         """
-        username, password = cls.__validate_insert_user(username=username, password=password)
+        username, password = self.__validate_insert_user(username=username, password=password)
 
         return Users(
             id=fake.random_number(digits=5),
@@ -30,8 +41,7 @@ class UserRepositorySpy(UserRepositoryInterface):
             # it's a test and wouldn't work with a real DB.
         )
 
-    @classmethod
-    def select_user(cls, user_id: int = None, username: str = None) -> List[Users]:
+    def select_user(self, user_id: int = None, username: str = None) -> List[Users]:
         """
         Method to test query data in user entity by ID and/or Name
         :param user_id: ID of the registry,
@@ -39,37 +49,52 @@ class UserRepositorySpy(UserRepositoryInterface):
         :return: List with Users selected
         """
 
-        cls.__validate_select_user(user_id, username)
+        self.__validate_select_user(user_id, username)
 
         if user_id and not username:
-            return [Users(
-                id=user_id,
-                username=fake.name(),
-                password='teste123',
-                register_date=now,
-                session='test.test.test'  # Instead of provide an engine, this feature offers "test.test.test"
-                # to kwon it's a test and wouldn't work with a real DB.
-            )]
+            try:
+                if user_id == self.id:
+                    return [Users(
+                        id=self.id,
+                        username=self.usename,
+                        password=self.password,
+                        register_date=self.register_date,
+                        session=self.session,
+                    )]
+                else:
+                    raise Exception
+            except Exception:
+                raise NoResultFoundError(message='No row was found when one was required', code=None)
 
         elif not user_id and username:
-            return [Users(
-                id=fake.random_number(digits=5),
-                username=username,
-                password='teste123',
-                register_date=now,
-                session='test.test.test'  # Instead of provide an engine, this feature offers "test.test.test"
-                # to kwon it's a test and wouldn't work with a real DB.
-            )]
+            try:
+                if username == self.usename:
+                    return [Users(
+                        id=self.id,
+                        username=self.usename,
+                        password=self.password,
+                        register_date=self.register_date,
+                        session=self.session,
+                    )]
+                else:
+                    raise Exception
+            except Exception:
+                raise NoResultFoundError(message='No row was found when one was required', code=None)
 
         elif user_id and username:
-            return [Users(
-                id=user_id,
-                username=username,
-                password='teste123',
-                register_date=now,
-                session='test.test.test'  # Instead of provide an engine, this feature offers "test.test.test"
-                # to kwon it's a test and wouldn't work with a real DB.
-            )]
+            try:
+                if username == self.usename and user_id == self.id:
+                    return [Users(
+                        id=self.id,
+                        username=self.usename,
+                        password=self.password,
+                        register_date=self.register_date,
+                        session=self.session,
+                    )]
+                else:
+                    raise Exception
+            except Exception:
+                raise NoResultFoundError(message='No row was found when one was required', code=None)
 
     @classmethod
     def __validate_insert_user(cls, username, password):
